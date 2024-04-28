6a7764544ce965e3ae5ef323240f0f86d8cb3f57
luqmabbbbb <luqmannfauzy46@gmail.com>
2024-04-28 08:27:28 +0800

    first commit

---
+++ js.js
@@ -0,0 +1,19 @@
+let schedules = [
+    {
+        startTime: '07:00',
+        endTime: '08:00',
+        task: 'belajar HTML'
+    },
+    {
+        startTime: '08:00',
+        endTime: '09:00',
+        task: 'belajar CSS'
+    },
+    {
+        startTime: '09:00',
+        endTime: '16:00',
+        task: 'membuat '
+    },
+]
+schedules = schedules.slice(1,2)
+console.log(schedules)
